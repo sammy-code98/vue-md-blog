e4eaaf1d636b8dcc5506d3785f61b71b818cdca6
uzor samuel ugochukwu <sammysmart98@yahoo.com>
2021-08-09 11:50:45 +0100

    updated

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="flex flex-col items-center align-center">
+    <div class="flex flex-col items-center align-center relative">
+                <router-link to="/" tag="a" class="bg-gray-100 rounded-md p-2 m-4 font-bold  left-0 top-2.5 hover:bg-gray-200 fixed">Back</router-link>
+
         <router-view/>
-        <router-link to="/" tag="a" >Back</router-link>
     </div>
 </template>
