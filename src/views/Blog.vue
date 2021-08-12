267491c4d6567ae7d7549da49e9324ecac0edf7a
uzor samuel ugochukwu <sammysmart98@yahoo.com>
2021-08-12 22:59:02 +0100

    fixed margin on back button

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -8,10 +8,11 @@
         bg-gray-100
         rounded-md
         p-2
-        m-4
+        my-12
+        mx-6
         font-bold
         left-0
-        top-2.5
+        top-3.5
         hover:bg-gray-200
         fixed
       "
