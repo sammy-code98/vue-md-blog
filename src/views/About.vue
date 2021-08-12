541acbd80e7cbf3da567111fea59a56c05e387ec
uzor samuel ugochukwu <sammysmart98@yahoo.com>
2021-08-12 23:43:54 +0100

    about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="m-4 font-bold text-xl">This is an about page</h1>
+    <p class="text-center tracking-wider leading-7 mx-5 p-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla placerat convallis tellus, ac aliquam quam tristique quis. Morbi tincidunt sit amet odio maximus ornare. Ut placerat libero ut nisi convallis suscipit. Proin eget felis bibendum, viverra ipsum quis, mollis orci. Etiam ac scelerisque magna, nec ultricies enim. Etiam iaculis et enim eu scelerisque. Morbi eget neque feugiat, dignissim felis vitae, congue libero. Sed pellentesque cursus urna sed gravida. Vestibulum eu turpis sit amet lacus congue condimentum vestibulum id enim. Aliquam tristique vitae arcu sed pretium. Cras leo tortor, sollicitudin auctor quam quis, tincidunt porta leo. Nullam dapibus nisl mi. Pellentesque sem elit, molestie quis velit ut, aliquet hendrerit nunc</p>
+    <p class="text-center tracking-wider leading-7 mx-5 p-4">Duis eu tortor porta, congue sem id, porta nisi. Integer placerat neque non pretium tincidunt. Ut iaculis tincidunt eros non vestibulum. Aliquam id tincidunt dui. Nunc lobortis a nulla vel interdum. Cras auctor, ex nec eleifend laoreet, ex ante vehicula velit, quis aliquam ipsum nulla eget elit. Mauris dignissim ipsum sit amet mauris porttitor, ac sodales mi ultricies. Nam lacinia urna nunc, at vehicula erat viverra vel. Cras consequat leo et nisi scelerisque luctus.</p>
+    <div class="m-3">
+      <span class="my-2 font-normal text-gray-500 text-center">Subscribe to our newsletter</span>
+      <form>
+        <input class="border-2 rounded-sm mx-2 px-3 focus:border-gray-900 " type="email">
+        <button class="bg-gray-900 rounded-md p-1 font-bold text-white px-4 mx-3 hover:text-gray-900 hover:bg-white">Submit</button>
+      </form>
+    </div>
+
   </div>
 </template>
