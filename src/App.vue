6ae074c1561db48c0fef544a1ba45226b694e1fa
uzor samuel ugochukwu <sammysmart98@yahoo.com>
2021-08-12 22:56:24 +0100

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
   <div id="app" class="bg-gray-50">
     <div id="nav" class="sticky top-0 border shadow-sm bg-gray-50">
+      <span class="md:flex md:self-start md:italic text-center font-bold">Markdown-Blog</span>
+      <br/>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
 
+    <footer class="flex flex-row justify-center  bg-gray-900 text-white ">
+      <div class="my-2">
+        <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+            <p class="my-3">All rights reserved @ 2021</p>
+            <p class="text-red-500 px-5">Disclaimer: Information/Data on the site is for test purpose</p>
+
+      </div>
+
+      
+    </footer>
   </div>
 </template>
 <script>
-export default {
-  
-};
+export default {};
 </script>
 
 <style lang="scss">
